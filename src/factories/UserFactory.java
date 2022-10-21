84c992ecec16bb0e2afff895bdd57d7357281b6d
Nikolás Rodríguez <2002nikoniko2002@gmail.com>
2022-10-21 08:43:06 +0200

    Finished a usable Application class and UserFactory, from this point on, the interface Userable is also complete

--- src/factories/UserFactory.java
+++ src/factories/UserFactory.java
@@ -1,9 +1,17 @@
 package factories;
 
+import dataAccess.DAOServer;
+import interfaces.Userable;
+
 /**
  *
  * @author Nicolas Rodriguez
  */
 public class UserFactory {
 
+    private static Userable user = new DAOServer();
+
+    public static Userable getAccessUser() {
+        return user;
+    }
 }
