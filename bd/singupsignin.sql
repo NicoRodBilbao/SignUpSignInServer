d97c29976773c439f592dd902eb49a83e16b299a
2dam <2dam@TartangaLH.eus>
2022-11-11 08:37:32 +0100

    now the database script generates the database

---
+++ bd/singupsignin.sql
@@ -0,0 +1,88 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Win32 (AMD64)
+--
+-- Host: localhost    Database: signupsignin
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `signupsignin`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `signupsignin` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `signupsignin`;
+
+--
+-- Table structure for table `signin`
+--
+
+DROP TABLE IF EXISTS `signin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `signin` (
+  `lastSignIn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`userId`,`lastSignIn`),
+  CONSTRAINT `signin_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `usertolog` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `signin`
+--
+
+LOCK TABLES `signin` WRITE;
+/*!40000 ALTER TABLE `signin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `signin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usertolog`
+--
+
+DROP TABLE IF EXISTS `usertolog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usertolog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(30) NOT NULL DEFAULT '',
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `fullName` varchar(50) NOT NULL,
+  `status` enum('enabled','disabled') NOT NULL,
+  `privilege` enum('user','admin') NOT NULL,
+  `password` varchar(30) NOT NULL DEFAULT '',
+  `lastPasswordChange` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usertolog`
+--
+
+LOCK TABLES `usertolog` WRITE;
+/*!40000 ALTER TABLE `usertolog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usertolog` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-10 13:00:46
